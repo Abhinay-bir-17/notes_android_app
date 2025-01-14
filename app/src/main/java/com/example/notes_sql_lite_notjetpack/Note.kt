69483e3ac63a766448caeab36742496888dcf7ad
Abhinay-bir-17 <abhinaybirsingh10@gmail.com>
2025-01-14 21:32:17 +0530

    can store nd fetch from db, next is to update the note

---
+++ app/src/main/java/com/example/notes_sql_lite_notjetpack/Note.kt
@@ -0,0 +1,6 @@
+package com.example.notes_sql_lite_notjetpack
+/*whenever we store data we create a dataclass*/
+data class Note(val id: Int, val title: String, val content:String)
+/*now we will create sql lite db and all queries so create notesdbhelper.kt file */
+
+
